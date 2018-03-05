69b90a6e1537bef1dcba1eed7dfa64dc6cd2c8c1
Durantk837 <Durantk837@gmail.com>
2018-03-05 16:23:07 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,109 @@
+<?php
+
+ ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Arbre Genealogique</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
+  </head>
+  <body>
+    <div class="container page">
+      <!--- Début arbre -->
+      <h1>GENEALOGIE</h1>
+
+      <div class="genealogie">
+        <ul>
+          <!-- ####### DEBUT GENEALOGIE ####### -->
+          <li><span class="couple"><a href="#">Pa KAMGA</a> <a href="#">Ma Anne</a></span>
+            <ul>
+              <li><span class="couple"><a href="#">Ma Francoise</a><a href="#">Pa Justin</a></span>
+                <ul>
+                  <li><a href="#">Aude</a></li><li><a href="#">Charlie</a></li>
+                </ul>
+              </li>
+              <li><span class="couple"><a href="#">Pa KENGNE Christian</a> <a href="#">Ma Rosette</a></span>
+                <ul>
+                  <li><span class="#"><a href="#">Yann Carel</a></span>
+                  </li>
+                  <li><a href="#">Steve jordan</a>
+                  </li>
+                  <li><a href="#">Nolita</a>
+                  </li>
+                  <li><a href="#">Sharonne</a>
+                  </li>
+                </ul>
+              </li>
+
+              <li><span class="couple"><a href="#">Pa Olivier</a><a href="#">Ma Veronique</a></span>
+                <ul>
+                      <li><a href="#">Jessica</a></li><li><a href="#">Guy</a></li><li><a href="#">Emi</a></li><li><a href="#">Winnie</a></li>
+                    </ul>
+              </li>
+              <li><span class="couple"><a href="#">Ma Bernadette</a><a href="#">Pa Samuel</a></span>
+                <ul>
+                  <li><a href="#">Francine</a></li>
+                  <li><span class="couple"><a href="#">Vanessa</a><a href="#">Jules</a></li>
+                  <li><a href="#">Kevin</a></li>
+                    <li><a href="#">Steven</a></li>
+                    <li><a href="#">Bill Nelson</a></li>
+                </ul>
+              </li>
+              <li><span class="couple"><a href="#">Pa Jean Noel</a><a href="#">Ma Marie</a></span>
+                <ul>
+                  <li><a href="#">Hance</a></li>
+                  <li><a href="#">Suzie</a></li>
+                </ul>
+              </li>
+              <li><span class="couple"><a href="#">Ma Justine</a><a href="#">Pa Jean-Pierre</a></span>
+                <ul>
+                  <li><a href="#">Dodo</a>
+                      <ul>
+                        <li><a href="#">Ulisse</a>
+                      </ul>
+                  </li>
+                  <li><a href="#">Lea</a></li>
+                  <li><a href="#">Valdis</a></li>
+                  <li><a href="#">VisClair</a></li>
+                </ul>
+              </li>
+              <li><span class="couple"><a href="#">Ma Germaine</a><a href="#">Pa Samuel</a></span>
+                <ul>
+                  <li><a href="#">Urich</a></li><li><a href="#">Irina</a></li><li><a href="#">Jessica</a></li><li><a href="#">Jeni</a></li><li><a href="#">John</a></li>
+                </ul>
+              </li>
+              <li><span class="couple"><a href="#">Ta Rachele</a><a href="#">Ton Charlin</a></span>
+                <ul>
+                  <li><a href="#">Tatiana</a></li>
+                  <li><a href="#">Roel</a></li>
+                  <li><a href="#">Samira</a></li>
+                  <li><a href="#">Berny</a></li>
+                </ul>
+              </li>
+              <li><span class="couple"><a href="#">Ton Raoul</a><a href="#">Ta Linda</a></span>
+                <ul>
+                  <li><a href="#">Roy</a></li>
+                  <li><a href="#">Elicha</a></li>
+                </ul>
+              </li>
+              <li><span class="couple"><a href="#">Ta Nelly</a><a href="#">Ton Marcellin</a></span>
+                <ul>
+                  <li><a href="#">Prisca</a></li>
+                  <li><a href="#">Anna</a></li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+          <!-- ####### FIN GENEALOGIE ####### -->
+        </ul>
+      </div>
+    <!-- Fin arbre -->
+    </div>
+
+
+    <!-- JAVASCRIPT Bootstrap -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  </body>
+</html>
